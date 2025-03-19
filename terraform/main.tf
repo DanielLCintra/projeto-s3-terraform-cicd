1ce249c3480cf1beca14331b8e3d127409d69d99
Daniel Loureiro Cintra <danielloureirocintra@Daniels-MacBook-Air-2.local>
2025-03-19 12:37:37 -0300

    melhorando o deploy para caso o bucket ja exista nao tentar recriar

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,36 +2,45 @@ provider "aws" {
   region = var.aws_region
 }
 
-# Verifica se o bucket existe antes de tentar criá-lo
-data "aws_s3_bucket" "existing_bucket" {
-  bucket = var.bucket_name
+# Usa try/catch para verificar se o bucket existe
+locals {
+  bucket_exists = can(jsondecode(data.aws_cli_command.check_bucket.result)["exists"])
+  bucket_id     = var.bucket_name
+}
+
+# Usa AWS CLI para verificar se o bucket existe
+data "aws_cli_command" "check_bucket" {
+  lifecycle {
+    postcondition {
+      condition     = true
+      error_message = "This will never fail"
+    }
+  }
   
-  # Ignora erros se o bucket não existir
-  # Isso permitirá que o Terraform continue e crie o bucket
-  count = 0
+  command = "s3api"
+  
+  arguments = {
+    "head-bucket" = ""
+  }
+  
+  query = "{ \"exists\": true }"
+  
+  cli_inputs = {
+    "bucket" = var.bucket_name
+  }
 }
 
 # Cria o bucket apenas se ele não existir
 resource "aws_s3_bucket" "website" {
-  bucket = var.bucket_name
+  count = local.bucket_exists ? 0 : 1
   
-  # Como a consulta data tem count = 0, ela nunca existirá,
-  # mas usamos uma expressão condicional no count para criar
-  # um novo bucket apenas se ele não existir
-  count = 0 == 0 ? 1 : 0
+  bucket        = var.bucket_name
+  force_destroy = true
   
   tags = {
     Name        = "Website Bucket"
     Environment = var.environment
   }
-  
-  # Torna o bucket compatível com o modo destroy
-  force_destroy = true
-}
-
-# Recurso local para referenciar o bucket, seja existente ou recém-criado
-locals {
-  bucket_id = try(aws_s3_bucket.website[0].id, var.bucket_name)
 }
 
 # Configuração para hospedagem de site
@@ -45,9 +54,6 @@ resource "aws_s3_bucket_website_configuration" "website_config" {
   error_document {
     key = "error.html"
   }
-  
-  # Depende do recurso do bucket apenas se ele for criado
-  depends_on = [aws_s3_bucket.website]
 }
 
 # Torna o bucket público
@@ -58,16 +64,12 @@ resource "aws_s3_bucket_public_access_block" "website_public_access" {
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
-  
-  # Depende do recurso do bucket apenas se ele for criado
-  depends_on = [aws_s3_bucket.website]
 }
 
 # Política de permissão para acesso público aos arquivos do bucket
 resource "aws_s3_bucket_policy" "website_policy" {
   bucket = local.bucket_id
   
-  # Garante que o bloco de acesso público seja aplicado primeiro
   depends_on = [aws_s3_bucket_public_access_block.website_public_access]
   
   policy = jsonencode({
@@ -97,7 +99,4 @@ resource "aws_s3_bucket_cors_configuration" "website_cors" {
     allowed_origins = ["*"]
     max_age_seconds = 3000
   }
-  
-  # Depende do recurso do bucket apenas se ele for criado
-  depends_on = [aws_s3_bucket.website]
 }
